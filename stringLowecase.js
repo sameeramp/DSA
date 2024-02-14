4199ffbe87a6528abf5c491e15015b4697caa421
sameeramp <sameeramp2000@gmail.com>
2024-02-14 22:51:33 +0530

    string-1

---
+++ stringLowecase.js
@@ -0,0 +1,11 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+let s = "Hello"
+let val= s.toLowerCase();
+return val; 
