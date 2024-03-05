0225f9725d1849d6e0e0b6be062fc8871d9d7263
sameeramp <sameeramp2000@gmail.com>
2024-03-05 23:49:44 +0530

    new

---
+++ nw1.js
@@ -0,0 +1,19 @@
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+let str = 121
+for(let i=0;i<str.length/2;i++){
+  if(str[i]!=str[str.length-1-i]){
+     return false;
+  }
+}
+return true;
