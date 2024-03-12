a21d5093de6ec8bfd8d1e9e231b70f1d223c783c
sameeramp <sameeramp2000@gmail.com>
2024-03-12 23:18:44 +0530

    n

--- nw1.js
+++ nw1.js
@@ -1,19 +1,28 @@
-// Given an integer x, return true if x is a 
-// palindrome
-// , and false otherwise.
+// You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+// Return the sum of all the unique elements of nums.
 
  
 
 // Example 1:
 
-// Input: x = 121
-// Output: true
-// Explanation: 121 reads as 121 from left to right and from right to left.
-
-let str = 121
-for(let i=0;i<str.length/2;i++){
-  if(str[i]!=str[str.length-1-i]){
-     return false;
-  }
+// Input: nums = [1,2,3,2]
+// Output: 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+let nums = [1,2,3,2]
+let sum=0;
+for(let i=0;i<nums.length;i++){
+    let count=1;
+    if(nums[i]!=='-1'){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i]==nums[j]){
+            count++;
+            nums[j]='-1';
+        }
+    }
+    if(count==1){
+        sum+=nums[i];
+    }
+    }
 }
-return true;
+return sum;
