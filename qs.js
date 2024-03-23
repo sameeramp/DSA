5dd224a5413b85d8c2be3bcb1a7ee899885fd682
sameeramp <sameeramp2000@gmail.com>
2024-03-23 23:19:08 +0530

    new

---
+++ qs.js
@@ -0,0 +1,19 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+let s = "Hello World"
+s=s.trim();
+  let val= s.split(" ");
+ let res=(val[val.length-1])
+ let out=res.length;
+ return out;
